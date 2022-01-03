7f5331bcdcdf5fcad9553666e51fbfe80b505e1b
BrigittePowers <reyhn701@gmail.com>
2022-01-02 21:15:56 -0600

    Updated auth and server for apollo and graphql

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,13 +8,15 @@ module.exports = {
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // split bearer from tokenvalue
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+      // return error if no token
     if (!token) {
       return res.status(400).json({ message: 'You have no token!' });
     }

8d6b6fefa78a1c649f0c316650209894ad5ed3cd
BrigittePowers <reyhn701@gmail.com>
2022-01-02 21:06:09 -0600

    Refactoring initial code and setting up dependencies for Apollo and Graphql

--- server/server.js
+++ server/server.js
@@ -1,21 +1,48 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+// typeDef and resolver import
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// apollo
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
+});
+
+// middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+	app.listen(PORT, () =>
+		console.log(`🌍 Now listening on localhost:${PORT}`),
+	);
+	console.log('GraphQL @ http://localhost:${PORT}${server.graphqlPath');
+});
+
+process.on('uncaughtException', function (err) {
+	console.log('Caught exception: ' + err);
 });

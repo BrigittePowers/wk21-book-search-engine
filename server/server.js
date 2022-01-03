fc6920e1e0ee65550ad4db5fc94d06d5969405a0
BrigittePowers <reyhn701@gmail.com>
2022-01-03 12:54:14 -0600

    Updated server and app to support graphql new server info

--- server/server.js
+++ server/server.js
@@ -6,26 +6,22 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-// typeDef and resolver import
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// apollo
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 	context: authMiddleware,
 });
 
-// middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
 }
@@ -37,10 +33,12 @@ app.get('*', (req, res) => {
 app.use(routes);
 
 db.once('open', () => {
-	app.listen(PORT, () =>
-		console.log(`🌍 Now listening on localhost:${PORT}`),
-	);
-	console.log('GraphQL @ http://localhost:${PORT}${server.graphqlPath');
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+		console.log(
+			`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`,
+		);
+	});
 });
 
 process.on('uncaughtException', function (err) {
